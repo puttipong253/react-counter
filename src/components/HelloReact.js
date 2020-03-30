6666a9b2d41de4dee62113b30accf88563640e7b
PHUTHIPHONG MONGKHON <james_apple@hotmail.co.th>
2020-03-30 18:49:55 +0700

    first commit

---
+++ src/components/HelloReact.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+class HelloReact extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            counter: 0
+        }
+    }
+    onIncrement = () => {
+        this.setState(
+            state => ({
+                counter: this.state.counter + 1
+            })
+        )
+    }
+
+    onDecrement = () => {
+        this.setState(
+            state => ({
+                counter: this.state.counter - 1
+            })
+        )
+    }
+
+    onReset = () => {
+        this.setState(
+            state => ({
+                counter:  0
+            })
+        )
+    }
+    render(){
+        return (
+            <div>
+                {this.state.counter} <br/>
+                <button onClick={this.onIncrement}>Increment</button>
+                <button onClick={this.state.counter > 0 ? this.onDecrement : null}>Decrement</button>
+                <button onClick={this.onReset}>Reset</button>
+            </div>
+        )
+    }
+}
+export default HelloReact;
